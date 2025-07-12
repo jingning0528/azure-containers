0993594368a10479b12a6d4caa3a76d533f6f864
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-07-11 17:19:18 -0700

    fix outputs

--- infrastructure/api/outputs.tf
+++ infrastructure/api/outputs.tf
@@ -1,28 +1,7 @@
-output "container_app_fqdn" {
-  description = "FQDN of the Container App"
-  value       = azurerm_container_app.api.latest_revision_fqdn
-}
-
-output "container_app_name" {
-  description = "Name of the Container App"
-  value       = azurerm_container_app.api.name
-}
-
-output "container_app_environment_id" {
-  description = "ID of the Container App Environment"
-  value       = azurerm_container_app_environment.main.id
-}
-
-output "log_analytics_workspace_id" {
+ output "log_analytics_workspace_id" {
   description = "ID of the Log Analytics Workspace"
   value       = azurerm_log_analytics_workspace.main.id
 }
-
-output "container_registry_login_server" {
-  description = "Login server of the Container Registry"
-  value       = var.create_container_registry ? azurerm_container_registry.main[0].login_server : null
-}
-
 output "user_assigned_identity_id" {
   description = "ID of the User Assigned Managed Identity"
   value       = azurerm_user_assigned_identity.container_apps.id
@@ -54,7 +33,7 @@ output "log_analytics_workspace_name" {
   description = "Name of the Log Analytics Workspace"
   value       = azurerm_log_analytics_workspace.main.name
 }
-
+/* 
 
 output "application_gateway_fqdn" {
   description = "FQDN of the Application Gateway public IP"
@@ -69,4 +48,4 @@ output "application_gateway_public_ip" {
 output "application_gateway_url" {
   description = "URL of the Application Gateway"
   value       = azurerm_public_ip.app_gateway.fqdn != null ? "https://${azurerm_public_ip.app_gateway.fqdn}" : "https://${azurerm_public_ip.app_gateway.ip_address}"
-}
+} */
